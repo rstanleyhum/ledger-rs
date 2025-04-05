17df6e86e21d4dd70afe94ad5c6c22874680f4f3
R. Stanley Hum <r.stanley.hum@gmail.com>
2025-04-05 03:09:57 -0400

    added commands to process raymond james export files

--- ledger-rs/src/main.rs
+++ ledger-rs/src/main.rs
@@ -1,8 +1,15 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, str::FromStr};
 
+use chrono::NaiveDate;
 use clap::{Parser, Subcommand};
 
 use ledger_rs_core::{parse::parse_filename, state::LedgerState};
+use ledger_rs_csv::{
+    rj_cdn::{compile_holdings, process_activites},
+    rj_cdn_closed::process_closed_acct_trans,
+    rj_symbols::load_symbols,
+    rj_usa::process_us_transaction,
+};
 
 #[derive(Parser)]
 #[command(version, about, long_about=None)]
@@ -13,18 +20,90 @@ struct Cli {
 
 #[derive(Subcommand, Debug)]
 enum Command {
-    Readall { filepath: PathBuf },
+    Bean {
+        filepath: PathBuf,
+    },
+    RjUsa {
+        filepath: PathBuf,
+        acct: String,
+        owner: String,
+        currency: String,
+    },
+    RjCdnClosed {
+        filepath: PathBuf,
+        acct: String,
+        owner: String,
+        currency: String,
+        commodity_f: PathBuf,
+    },
+    RjCdnActivities {
+        filepath: PathBuf,
+        acct: String,
+        owner: String,
+        currency: String,
+        symbol_f: PathBuf,
+    },
+    RjCdnHoldings {
+        filepath: PathBuf,
+        bkdate_string: String,
+        currency: String,
+    },
+    RjSymbols {
+        symbol_f: PathBuf,
+    },
 }
 
 fn main() {
     let cli = Cli::parse();
 
     match cli.command {
-        Command::Readall { filepath } => readall(filepath),
+        Command::Bean { filepath } => bean(filepath),
+        Command::RjUsa {
+            filepath,
+            acct,
+            owner,
+            currency,
+        } => rj_usa(filepath, acct.as_str(), owner.as_str(), currency.as_str()),
+        Command::RjCdnClosed {
+            filepath,
+            acct,
+            owner,
+            currency,
+            commodity_f,
+        } => rj_cdn_closed(
+            filepath,
+            acct.as_str(),
+            owner.as_str(),
+            currency.as_str(),
+            commodity_f,
+        ),
+        Command::RjCdnActivities {
+            filepath,
+            acct,
+            owner,
+            currency,
+            symbol_f,
+        } => rj_cdn_activites(
+            filepath,
+            acct.as_str(),
+            owner.as_str(),
+            currency.as_str(),
+            symbol_f,
+        ),
+        Command::RjCdnHoldings {
+            filepath,
+            bkdate_string,
+            currency,
+        } => rj_cdn_holdings(
+            filepath,
+            NaiveDate::from_str(&bkdate_string).unwrap(),
+            currency.as_str(),
+        ),
+        Command::RjSymbols { symbol_f } => rj_symbols(symbol_f),
     }
 }
 
-fn readall(f: PathBuf) {
+fn bean(f: PathBuf) {
     let mut state = LedgerState::new();
 
     state.insert(f.clone());
@@ -33,4 +112,75 @@ fn readall(f: PathBuf) {
 
     println!("tc_balances\n{}", state.tc_balances().unwrap());
     println!("cp_balances\n{}", state.cp_balances().unwrap());
+
+    state.write_transactions();
+}
+
+fn rj_usa(f: PathBuf, acct: &str, owner: &str, currency: &str) {
+    let mut state = LedgerState::new();
+
+    process_us_transaction(f.to_str().unwrap(), acct, owner, currency, &mut state).unwrap();
+
+    println!("transactions: {}", state.transactions.len());
+    println!("postings: {}", state.postings.len());
+    println!("balances: {}", state.verifications.len());
+    println!("\n");
+    state.write_transactions();
+}
+
+fn rj_cdn_closed(f: PathBuf, acct: &str, owner: &str, currency: &str, commodity_f: PathBuf) {
+    let mut state = LedgerState::new();
+
+    process_closed_acct_trans(
+        f.to_str().unwrap(),
+        acct,
+        owner,
+        currency,
+        commodity_f.to_str().unwrap(),
+        &mut state,
+    )
+    .unwrap();
+
+    println!("transactions: {}", state.transactions.len());
+    println!("postings: {}", state.postings.len());
+    println!("balances: {}", state.verifications.len());
+    println!("\n");
+    state.write_transactions();
+}
+
+fn rj_cdn_activites(f: PathBuf, acct: &str, owner: &str, currency: &str, commodity_f: PathBuf) {
+    let mut state = LedgerState::new();
+
+    process_activites(
+        f.to_str().unwrap(),
+        acct,
+        owner,
+        currency,
+        commodity_f.to_str().unwrap(),
+        &mut state,
+    )
+    .unwrap();
+
+    println!("transactions: {}", state.transactions.len());
+    println!("postings: {}", state.postings.len());
+    println!("balances: {}", state.verifications.len());
+    println!("\n");
+    state.write_transactions();
+}
+
+fn rj_cdn_holdings(f: PathBuf, bkdate: NaiveDate, currency: &str) {
+    let mut state = LedgerState::new();
+
+    compile_holdings(f.to_str().unwrap(), bkdate, currency, &mut state).unwrap();
+
+    println!("transactions: {}", state.transactions.len());
+    println!("postings: {}", state.postings.len());
+    println!("balances: {}", state.verifications.len());
+    println!("\n");
+    state.write_transactions();
+}
+
+fn rj_symbols(f: PathBuf) {
+    let result = load_symbols(String::from(f.to_str().unwrap())).unwrap();
+    println!("{:?}", result);
 }
